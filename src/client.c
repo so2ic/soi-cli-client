3b6fe51ab0bafb318226ff80abd8def7e371e69e
vincentastolfi <vincent.astolfi@etu.uca.fr>
2025-03-17 22:46:19 +0100

    feat: ber

--- src/client.c
+++ src/client.c
@@ -8,9 +8,11 @@ void handler(int sock)
     printf("Waiting for other player\n");
 
     {
-        meta_t packet;
-        recv(sock, &packet, sizeof(meta_t), 0);
-        if(packet.type == 0x01)
+        meta_t recv_packet;
+        meta_t send_packet = {.type = 0xFF, .size = 0};
+        recv(sock, &recv_packet, sizeof(meta_t), 0);
+        if(recv_packet.type == 0x01)
            printf("Ennemy found\n");
+        send(sock, &send_packet, sizeof(meta_t), 0);
     }
 }
